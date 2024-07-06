32da9d6b4a274bc0873dc35713930d3b9b206c13
Ihavenoideawhatimdoing1 <146613255+Ihavenoideawhatimdoing1@users.noreply.github.com>
2024-07-05 22:42:22 -0400

    Update FakePythonScraper.py

--- FakePythonScraper.py
+++ FakePythonScraper.py
@@ -4,35 +4,34 @@ import requests as rq
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# <h2 class="title is-5">Dispensing optician</h2>
-
+#Giving soup the URL
 url = 'https://realpython.github.io/fake-jobs/'
 webpage = rq.get(url)
 
+# Appended Lists
 job_titles = []
 company_titles = []
 location_titles = []
 date_titles = []
 
-
+#Creating the data map
 data = {'Job titles': job_titles, 'Company titles': company_titles, 'Location Title': location_titles, "Date": date_titles}
 
-
+#Parsing the Website
 soup = bs(webpage.content, 'html.parser')
+
+#Finding Job Titles
 job_title_tags = soup.find_all('h2')
-
-
-#Job Titles
 for job in job_title_tags:
     job_titles.append(job.text.upper())
 
-#H3 Tags
+#Finding Company Names
 company_title_tags = soup.find_all('h3')
 
 for company in company_title_tags:
     company_titles.append(company.text.upper())
 
-
+# Finding Locations and cleaning the results
 location_title_tags = soup.find_all(class_='location')
 
 for location in location_title_tags:
@@ -47,6 +46,7 @@ for location in location_title_tags:
 
 date_title_tags = soup.find_all(class_='is-small has-text-grey')
 
+#Finding the dates and cleaning the results
 for date in date_title_tags:
     date_titles.append(
         date.text.upper()
@@ -54,11 +54,9 @@ for date in date_title_tags:
         .removesuffix('\n')
     )
 
-#print(date_titles)
-#print(len(date_titles))
-
-#print(soup.prettify)
+#Mapping/printing data to dataframe
 df = pd.DataFrame(data)
 print(df)
+#Exporting
 df.to_clipboard()
 df.to_csv
